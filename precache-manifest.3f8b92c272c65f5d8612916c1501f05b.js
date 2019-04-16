dadc9e99b28ae4a9e1654d8a4d8fd74d4843791c
Sevann Radhak T <sevann.radhak@gmail.com>
2019-04-16 08:26:37 -0500

    primer commit

---
+++ precache-manifest.3f8b92c272c65f5d8612916c1501f05b.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "0897949019caf8e324af",
+    "url": "/static/js/main.79eb7424.chunk.js"
+  },
+  {
+    "revision": "9eee67cf782a38d35aeb",
+    "url": "/static/js/2.d6a534e5.chunk.js"
+  },
+  {
+    "revision": "0897949019caf8e324af",
+    "url": "/static/css/main.4477a43b.chunk.css"
+  },
+  {
+    "revision": "7b4b6330eeb3aeb9684fe340a679830c",
+    "url": "/index.html"
+  }
+];
